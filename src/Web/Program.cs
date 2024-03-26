bd61d70373dc270bb5e676482fb4dc6f4a344b45
Zettai <raulbeltmarc@gmail.com>
2024-03-26 12:42:24 +0100

    add log to check where InitialiseDatabaseAsync is called

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -12,7 +12,14 @@ builder.Services.AddWebServices();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+Console.WriteLine("InitialiseDatabaseAsync----------------------------------------------------------------");
 await app.InitialiseDatabaseAsync();
+
+if (!app.Environment.IsDevelopment())
+{
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
 
 app.UseHealthChecks("/health");
 app.UseHttpsRedirection();
