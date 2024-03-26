9ec629e7bbc37bc518f1426b460a77b44af9683c
Zettai <raulbeltmarc@gmail.com>
2024-03-26 12:16:02 +0100

    apply migrations to non-development envs

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -12,11 +12,9 @@ builder.Services.AddWebServices();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    await app.InitialiseDatabaseAsync();
-}
-else
+await app.InitialiseDatabaseAsync();
+
+if (!app.Environment.IsDevelopment())
 {
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
