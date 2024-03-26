18148bda743ebd79bb8ca4ba185fe28a6d0bdda9
Zettai <raulbeltmarc@gmail.com>
2024-03-26 12:28:33 +0100

    test without hts

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -14,12 +14,6 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 await app.InitialiseDatabaseAsync();
 
-if (!app.Environment.IsDevelopment())
-{
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
 app.UseHealthChecks("/health");
 app.UseHttpsRedirection();
 app.UseStaticFiles();
