3a233c0c29f1830e3e8b224864bef412575a16a4
Zettai <raulbeltmarc@gmail.com>
2024-03-27 10:03:59 +0100

    trying env variables approach

--- src/Infrastructure/DependencyInjection.cs
+++ src/Infrastructure/DependencyInjection.cs
@@ -14,7 +14,7 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
     {
-        var connectionString = configuration.GetConnectionString("DefaultConnection");
+        var connectionString = configuration.GetConnectionString("DefaultConnection") ?? Environment.GetEnvironmentVariable("CONNECTION_STRING");
 
         Guard.Against.Null(connectionString, message: "Connection string 'DefaultConnection' not found.");
 
