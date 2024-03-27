ab9c13b99d0d17ef6515e111523279b0c65befa2
Zettai <raulbeltmarc@gmail.com>
2024-03-27 11:50:17 +0100

    try to fix tests (in local there is no problem)

--- src/Infrastructure/DependencyInjection.cs
+++ src/Infrastructure/DependencyInjection.cs
@@ -14,25 +14,8 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
     {
-        var connStr = Environment.GetEnvironmentVariable("APPSETTING_CONNECTION_STRING");
-        if (string.IsNullOrEmpty(connStr))
-        {
-            Console.WriteLine("CONNECTION_STRING environment variable is not set.");
-        }
-
-        var connStr2 = Environment.GetEnvironmentVariable("CONNECTION_STRING");
-        if (string.IsNullOrEmpty(connStr2))
-        {
-            Console.WriteLine("CONNECTION_STRING environment variable is not set.");
-        }
-
-        var connectionString = configuration.GetConnectionString("DefaultConnection");
-        if (string.IsNullOrEmpty(connectionString))
-        {
-            Console.WriteLine("'DefaultConnection' not found, looking for environment variable ");
-            connectionString = Environment.GetEnvironmentVariable("APPSETTING_CONNECTION_STRING")
-                            ?? Environment.GetEnvironmentVariable("CONNECTION_STRING");
-        }
+        var connectionString = configuration.GetConnectionString("DefaultConnection")
+            ?? Environment.GetEnvironmentVariable("APPSETTING_CONNECTION_STRING");
 
         Guard.Against.Null(connectionString, message: "Connection string 'DefaultConnection' not found.");
 
