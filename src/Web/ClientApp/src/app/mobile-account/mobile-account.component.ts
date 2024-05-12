b97d43877f19c205a9c51b88f4632211498904d1
Zettai <raulbeltmarc@gmail.com>
2024-05-12 20:06:30 +0200

    refactor styles

--- src/Web/ClientApp/src/app/mobile-account/mobile-account.component.ts
+++ src/Web/ClientApp/src/app/mobile-account/mobile-account.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-mobile-account',
   templateUrl: './mobile-account.component.html',
-  styleUrl: './mobile-account.component.css'
 })
 export class MobileAccountComponent {
   static key = 'account';
--- src/Web/ClientApp/src/app/mobile-items/mobile-items.component.ts
+++ src/Web/ClientApp/src/app/mobile-items/mobile-items.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-mobile-items',
-  templateUrl: './mobile-items.component.html',
-  styleUrl: './mobile-items.component.css'
+  templateUrl: './mobile-items.component.html'
 })
 export class MobileItemsComponent {
   static key = 'items';
