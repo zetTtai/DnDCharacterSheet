715cf07d493da62ec5874e0cc7918f84dc5df691
Zettai <raulbeltmarc@gmail.com>
2024-07-25 08:38:19 +0200

    add convert buttons (mobile)

--- src/Web/ClientApp/src/app/modules/layout-components.module.ts
+++ src/Web/ClientApp/src/app/modules/layout-components.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { TranslationModule } from './translation.module';
+import { SharedModule } from './shared.module';
+
 
 import { AbilitiesComponent } from 'src/app/components/abilities/abilities.component';
 import { DeathSavesComponent } from 'src/app/components/death-saves/death-saves.component';
@@ -24,7 +26,8 @@ import { NotesComponent } from 'src/app/components/notes/notes.component';
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    TranslationModule
+    TranslationModule,
+    SharedModule
   ],
   exports: [
     AbilitiesComponent,
